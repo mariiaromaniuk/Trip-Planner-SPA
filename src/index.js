458febdcd7a1c990a19b19cbd136280d18757e02
Ousainu Jabbi <ousainujabbi@gmail.com>
2020-05-04 15:37:39 -0400

    changes to trip planner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hi there!');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1Ijoib3VzYWludSIsImEiOiJjazlzc29pcnYxOGxzM25xcW9hazRjamdpIn0.3uDInIYPDhukyR9fZyQ5ow";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
